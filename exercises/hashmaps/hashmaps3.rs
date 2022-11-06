a1bd43a44a1ddabf3b97950f67e3acff1ff11567
Shadow-Dream <1820636951@qq.com>
2022-11-06 20:49:30 +0800

    modified:   exercises/hashmaps/hashmaps1.rs 	modified:   exercises/hashmaps/hashmaps2.rs 	modified:   exercises/hashmaps/hashmaps3.rs 	modified:   exercises/modules/modules1.rs 	modified:   exercises/modules/modules2.rs 	modified:   exercises/modules/modules3.rs 	modified:   exercises/quiz2.rs 	modified:   exercises/strings/strings1.rs 	modified:   exercises/strings/strings2.rs 	modified:   exercises/strings/strings3.rs 	modified:   exercises/strings/strings4.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -40,6 +40,22 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team_1 = Team {
+            name:team_1_name,
+            goals_scored:0,
+            goals_conceded:0
+        };
+        let team_2 = Team{
+            name:team_2_name,
+            goals_scored:0,
+            goals_conceded:0
+        };
+        let update = scores.entry(v[0].to_string()).or_insert(team_1);
+        (*update).goals_scored+=team_1_score;
+        (*update).goals_conceded+=team_2_score;
+        let update = scores.entry(v[1].to_string()).or_insert(team_2);
+        (*update).goals_scored+=team_2_score;
+        (*update).goals_conceded+=team_1_score;
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,7 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -30,11 +30,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command{
+                Command::Uppercase=>{
+                    output.push(string.to_uppercase());
+                },
+                Command::Trim=>{
+                    output.push(String::from(string.trim()));
+                },
+                Command::Append(size)=>{
+                    let mut string = String::from(string);
+                    for _ in 1..(size+1){
+                        string.push_str("bar");
+                    }
+                    output.push(string);
+                }
+            }
         }
         output
     }
@@ -43,7 +58,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
